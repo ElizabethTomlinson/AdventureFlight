726c891eb7dd36f0e2cf7bf8d592bd1b3bdc0664
elizabeth <midlink545@gmail.com>
2023-03-05 09:38:02 -0800

    Moved some constants to Constants.h

--- Constants.h
+++ Constants.h
@@ -5,5 +5,7 @@
 #ifndef ADVENTUREFLIGHT_CONSTANTS_H
 #define ADVENTUREFLIGHT_CONSTANTS_H
 
-static constexpr double DEFAULT_FUEL_GENERATION_RATE = 1.0;
+static constexpr double DEFAULT_FUEL_GENERATION_RATE = 2.0;
+static constexpr double DEFAULT_INITIAL_FUEL = 5000;
+static constexpr int AIRCRAFT_NUM = 5;
 #endif //ADVENTUREFLIGHT_CONSTANTS_H
--- GameManager.cpp
+++ GameManager.cpp
@@ -22,7 +22,10 @@ void GameManager::startNewGame() {
         cmd = readNextCommand();
         if (cmd != "exit") {
             int num = std::stoi(cmd);
-            this->game = new AdventureFlightGame(AdventureFlightSettings(5, 5000), coords.at(num - 1));
+            this->game = new AdventureFlightGame(AdventureFlightSettings(
+                                                         AIRCRAFT_NUM,
+                                                         DEFAULT_INITIAL_FUEL),
+                                                 coords.at(num - 1));
             this->next_action = STATUS;
         } else {
             exit(0);
