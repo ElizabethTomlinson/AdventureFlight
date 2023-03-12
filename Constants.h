3d0911d3db781b62992ee6a8aabc66fa0fe34835
elizabeth <midlink545@gmail.com>
2023-03-12 13:21:52 -0700

    Raised fuel gen rate

--- Constants.h
+++ Constants.h
@@ -5,7 +5,7 @@
 #ifndef ADVENTUREFLIGHT_CONSTANTS_H
 #define ADVENTUREFLIGHT_CONSTANTS_H
 
-static constexpr double DEFAULT_FUEL_GENERATION_RATE = 1.0;
+static constexpr double DEFAULT_FUEL_GENERATION_RATE = 1.2225;
 static constexpr double DEFAULT_INITIAL_FUEL = 10000;
 static constexpr int AIRCRAFT_NUM = 5;
 #endif //ADVENTUREFLIGHT_CONSTANTS_H
